fc7d3e14ed09b12d751fbcaf71025845b29c42d1
adarlpz <adarlopz.dev@gmail,com>
2025-04-19 16:17:15 -0600

    avances y mas avances

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ export default {
         './src/**/*.{html,js,astro,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#18cda9',
+                secondary: '#198980',
+                neutral: '#2d504a',
+
+                background: '#000f17',
+                backgroundPrimary: '#002a2e',
+                backgroundSecondary: '#003b41',
+            },
+        },
     },
     plugins: [],
 }
