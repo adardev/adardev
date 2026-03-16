c4f22b4d10b3bf09a2158922bafd77381ee0a225
adardev <adarlopz.dev@gmail.com>
2026-03-16 16:29:54 -0600

    correccion

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -52,24 +52,58 @@ export const projects = [
 
 export const skillCategories = [
     {
-        title: "Frontend", i18nTitle: "skillsFrontend",
+        title: "Front", i18nTitle: "skillsFront",
         skills: [
-            { name: "HTML", icon: "/logos/html5_logo.svg", i18nExp: "skillHtmlExp" },
-            { name: "CSS", icon: "/logos/css3_logo.svg", i18nExp: "skillCssExp" },
-            { name: "Tailwind", icon: "/logos/tailwind_logo.svg", i18nExp: "skillTailwindExp" },
+            { name: "HTML5", icon: "/logos/html5_logo.svg", i18nExp: "skillHtmlExp" },
+            { name: "CSS3", icon: "/logos/css3_logo.svg", i18nExp: "skillCssExp" },
             { name: "JavaScript", icon: "/logos/js_logo.svg", i18nExp: "skillJsExp" },
-            { name: "Astro", icon: "/logos/astro_logo.svg", i18nExp: "skillAstroExp" },
             { name: "React", icon: "/logos/react_logo.svg", i18nExp: "skillReactExp" },
+            { name: "Astro", icon: "/logos/astro_logo.svg", i18nExp: "skillAstroExp" },
+            { name: "Tailwind", icon: "/logos/tailwind_logo.svg", i18nExp: "skillTailwindExp" },
+            { name: "Bootstrap", icon: "/logos/bootstrap_logo.svg", i18nExp: "skillBootstrapExp" },
         ],
     },
     {
-        title: "Backend & Móvil", i18nTitle: "skillsBackend",
+        title: "Back", i18nTitle: "skillsBack",
         skills: [
             { name: "Node.js", icon: "/logos/nodejs_logo.svg", i18nExp: "skillNodejsExp" },
             { name: "PHP", icon: "/logos/php_logo.svg", i18nExp: "skillPhpExp" },
             { name: "Python", icon: "/logos/python_logo.svg", i18nExp: "skillPythonExp" },
-            { name: "Dart", icon: "/logos/dart_logo.svg", i18nExp: "skillDartExp" },
+            { name: "Django", icon: "/logos/django_logo.svg", i18nExp: "skillDjangoExp" },
             { name: "Flutter", icon: "/logos/flutter_logo.svg", i18nExp: "skillFlutterExp" },
+            { name: "Dart", icon: "/logos/dart_logo.svg", i18nExp: "skillDartExp" },
+            { name: "Electron", icon: "/logos/electron_logo.svg", i18nExp: "skillElectronExp" },
+            { name: "C++", icon: "/logos/cpp_logo.svg", i18nExp: "skillCppExp" },
+            { name: "C#", icon: "/logos/csharp_logo.svg", i18nExp: "skillCsharpExp" },
+            { name: "Java", icon: "/logos/java_logo.svg", i18nExp: "skillJavaExp" },
+            { name: "Kotlin", icon: "/logos/kotlin_logo.svg", i18nExp: "skillKotlinExp" },
+        ],
+    },
+    {
+        title: "BD", i18nTitle: "skillsBD",
+        skills: [
+            { name: "SQL", icon: "/logos/sql_logo.svg", i18nExp: "skillSqlExp" },
+            { name: "Supabase", icon: "/logos/supabase_logo.svg", i18nExp: "skillSupabaseExp" },
+            { name: "Firebase", icon: "/logos/firebase_logo.svg", i18nExp: "skillFirebaseExp" },
+            { name: "MongoDB", icon: "/logos/mongodb_logo.svg", i18nExp: "skillMongodbExp" },
+        ],
+    },
+    {
+        title: "Herramientas", i18nTitle: "skillsHerramientas",
+        skills: [
+            { name: "Git", icon: "/logos/git_logo.svg", i18nExp: "skillGitExp" },
+            { name: "Docker", icon: "/logos/docker_logo.svg", i18nExp: "skillDockerExp" },
+            { name: "Notion", icon: "/logos/notion_logo.svg", i18nExp: "skillNotionExp" },
+        ],
+    },
+    {
+        title: "Diseño", i18nTitle: "skillsDesign",
+        skills: [
+            { name: "Figma", icon: "/logos/figma_logo.svg", i18nExp: "skillFigmaExp" },
+            { name: "MockFlow", icon: "/logos/mockflow_logo.svg", i18nExp: "skillMockflowExp" },
+            { name: "Photoshop", icon: "/logos/photoshop_logo.svg", i18nExp: "skillPhotoshopExp" },
+            { name: "Illustrator", icon: "/logos/ilustrator_logo.svg", i18nExp: "skillIllustratorExp" },
+            { name: "Adobe XD", icon: "/logos/xd_logo.svg", i18nExp: "skillXdExp" },
         ],
     },
 ];
