e1a54c626c0b152317f6cfaa834c165ec6147dec
adardev <adarlopz.dev@gmail.com>
2026-03-18 18:15:22 -0600

    ubi link y deploy script

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -2,6 +2,6 @@ export const socialLinks = [
     { link: "https://github.com/adardev", iconSrc: "/icons/github_logo.svg", label: "/adardev" },
     { link: "https://www.linkedin.com/in/adardev", iconSrc: "/icons/linkedin_logo.svg", label: "/in/adardev" },
     { link: "https://www.youtube.com/@adardev1", iconSrc: "/icons/youtube_logo.svg", label: "/@adardev1" },
-    { link: null, iconSrc: "/icons/map_icon.svg", label: "Guadalajara, México" },
+    { link: "https://maps.app.goo.gl/xktv2qehn6sLSTFQ8", iconSrc: "/icons/map_icon.svg", label: "Guadalajara, México" },
     { link: "tel:+525665886939", iconSrc: "/icons/phone_icon.svg", label: "+52 33 3036 2181" },
 ];
